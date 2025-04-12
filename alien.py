be4bf328e5044d7b728525643ca99f732e440e37
Brianna Jackson <bjackson59@student.cscc.edu>
2025-04-11 23:50:21 -0400

    P2 - created a single column of stationary aliens for alien fleet

--- alien.py
+++ alien.py
@@ -3,15 +3,15 @@ from pygame.sprite import Sprite
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from alien_invasion import AlienInvasion
+    from alien_fleet import AlienFleet
 
 class Alien(Sprite):
-    def __init__(self, game: 'AlienInvasion', x: float, y: float):
+    def __init__(self, fleet: 'AlienFleet', x: float, y: float):
         super().__init__()
 
-        self.screen = game.screen
-        self.boundaries = game.screen.get_rect()
-        self.settings = game.settings
+        self.screen = fleet.game.screen
+        self.boundaries = fleet.game.screen.get_rect()
+        self.settings = fleet.game.settings
 
         self.image = pygame.image.load(self.settings.alien_file)
         self.image = pygame.transform.scale(self.image, (self.settings.alien_w,self.settings.alien_h))
@@ -19,7 +19,7 @@ class Alien(Sprite):
         self.image = pygame.transform.rotate(self.image, -90)
 
         self.rect = self.image.get_rect()
-        self.rect.midtop = game.ship.rect.midtop
+        self.rect.midtop = fleet.game.ship.rect.midtop
         self.rect.x = x
         self.rect.y = y
 
